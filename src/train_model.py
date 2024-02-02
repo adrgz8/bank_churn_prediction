786ceaad26d8c60e434852ee7c00d81a955e0d34
adrianrdz8 <adrian.rodriguez@udem.edu>
2024-02-02 18:03:07 +0100

    creating cleaning python file

--- src/train_model.py
+++ src/train_model.py
@@ -13,3 +13,4 @@ from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score, confusion_matrix, roc_curve
 from sklearn.utils.class_weight import compute_sample_weight
+
